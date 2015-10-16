9418b8472318d356496f7a72ab864d11f86ff70b
wuliang <wuliang@Ctrip.com>
2015-10-16 18:30:30 +0800

    add project

---
+++ GM/GM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GM.Startup))]
+namespace GM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
